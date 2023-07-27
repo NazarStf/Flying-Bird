3774fcdaff102a34e5c65766f8fa21403e8e8a6e
NazarStf <hworker98@gmail.com>
2023-07-27 23:02:41 +0300

    Initial Commit

---
+++ Flying Bird/Flying_BirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flying_BirdApp.swift
+//  Flying Bird
+//
+//  Created by NazarStf on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Flying_BirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
